0964975b1958cc5afb4d25ae8d95601d652e1e7d
[shotauchi] <[tianneixiang84@gmail.com]>
2024-11-14 17:21:41 +0000

    コミット03

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,16 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create','add');
-    Route::get('profile/edit','edit');
+    Route::get('profile/create','add')->middleware('auth');
+    Route::get('profile/edit','edit')->middleware('auth');
 });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
